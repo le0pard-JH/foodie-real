b81f740a0b23c9450d045c5714113b93a3ee2ba7
Joo-Ho, Hwang <orange_yellow@naver.com>
2020-12-02 15:12:49 +0900

    This is first github test about foodie.

---
+++ foodie-real/src/foodie_try_one/FoodieController.java
@@ -0,0 +1,12 @@
+package foodie_try_one;
+
+public class FoodieController {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    
+    System.out.println("[0] Foodie GitHub Connection Test 1");
+
+  }
+
+}
